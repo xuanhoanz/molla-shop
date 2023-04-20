13bca87731f2d32ed98b60c12d4e8579aee3e797
xuanhoanz <xuanhoanz@outlook.com>
2023-04-21 00:08:23 +1200

    updated home slider

--- resources/views/frontend/home/intro-slider.blade.php
+++ resources/views/frontend/home/intro-slider.blade.php
@@ -12,31 +12,14 @@
                             }
                         }'>
                 <div class="intro-slide"
-                     style="background-image: url({{asset('layouts/images/demos/demo-31/slider/slide-4.PNG')}}); background-color: #f8f8f8;">
-{{--                    <div class="intro-content text-center" >--}}
-{{--                        <h1 class="intro-title" style="color: saddlebrown">Your Saffron<br>Your Calm</h1>--}}
-{{--                        <!-- End .h3 intro-subtitle -->--}}
-{{--                        <h3 class="intro-text text-white font-weight-normal">Relax and restore your body and--}}
-{{--                            mind with these wellness essentials.</h3><!-- End .intro-title -->--}}
-
-{{--                        <a href="/products" class="btn btn-secondary font-weight-semibold">--}}
-{{--                            <span>DISCOVER NOW</span>--}}
-{{--                        </a>--}}
-{{--                    </div><!--End .intro-content-->--}}
+                     style="background-image: url({{asset('layouts/images/demos/demo-31/slider/slide-4.png')}}); background-color: #f8f8f8;">
                 </div><!-- End .intro-slide -->
 
                 <div class="intro-slide"
                      style="background-image: url({{asset('layouts/images/demos/demo-31/slider/slide-2.jpeg')}}); background-color: #39c;">
-{{--                    <div class="intro-content text-center">--}}
-{{--                        <h1 class="intro-title text-white">Challenge Yourself</h1>--}}
-{{--                        <!-- End .h3 intro-subtitle -->--}}
-{{--                        <h3 class="intro-text text-white font-weight-normal">Tack your practice to the next--}}
-{{--                            level with on-going yoga challenges.</h3><!-- End .intro-title -->--}}
-
-{{--                        <a href="/products" class="btn btn-secondary font-weight-semibold">--}}
-{{--                            <span>DISCOVER NOW</span>--}}
-{{--                        </a>--}}
-{{--                    </div><!--End .intro-content-->--}}
+                </div><!-- End .intro-slide -->
+                <div class="intro-slide"
+                     style="background-image: url({{asset('layouts/images/demos/demo-31/slider/slide-1.jpeg')}}); background-color: #39c;">
                 </div><!-- End .intro-slide -->
             </div><!-- End .intro-slider owl-carousel owl-theme -->
 
